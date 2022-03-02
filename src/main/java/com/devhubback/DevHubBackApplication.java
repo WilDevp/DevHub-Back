8479a3e9dd759fac7df19e16235f4a4f7f8eeba1
Wilmar Darío García Valderrama <wilmar.garcia@novatec.loc>
2022-03-02 16:40:56 -0500

    first commit v2

---
+++ src/main/java/com/devhubback/DevHubBackApplication.java
@@ -0,0 +1,13 @@
+package com.devhubback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevHubBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevHubBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devhubback/DevHubBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhubback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHubBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
