96e5c2329e6867e352deaadbe826b515242da4e0
santiago <santiago.augusto48@gmail.com>
2022-03-14 10:41:43 -0500

    creacion UsuarioService

---
+++ src/main/java/com/devhubback/service/UsuarioService.java
@@ -0,0 +1,41 @@
+package com.devhubback.service;
+
+import com.devhubback.model.Usuario;
+import com.devhubback.repository.IUsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService implements IUsuarioService{
+
+    @Autowired
+    private IUsuarioRepository usuarioRepository;
+
+    @Override
+    public List<Usuario> findAll() {
+        return usuarioRepository.findAll();
+    }
+
+    @Override
+    public Optional<Usuario> findById(Integer id) {
+        return usuarioRepository.findById(id);
+    }
+
+    @Override
+    public Usuario create(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public Usuario update(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        usuarioRepository.deleteById(id);
+    }
+}
